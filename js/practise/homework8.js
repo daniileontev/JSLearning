f9bfebb7eba8be490d5dee8d31e12e9163020404
Daniil.Leontev <eDryE8EZx56HTpd>
2022-07-19 18:47:37 +0500

    добавил еще один вариант решения для фибоначи

--- js/practise/homework8.js
+++ js/practise/homework8.js
@@ -29,3 +29,21 @@ function fib(n) {
     return fn
 }
 console.log(fib(5)); 
+
+
+//еще одно решение без рекурсии:
+
+function fibonaci(num){
+    const result = [0, 1];
+
+    for(let i = 2; i <= num; i++){
+
+        const prevNumber1 = result[i - 1];
+        const prevNumber2 = result[i - 2];
+        result.push(prevNumber1+ prevNumber2);
+    }
+
+    return result[num];
+}
+
+console.log(fibonaci(7));
