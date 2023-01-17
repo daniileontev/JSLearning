21022413b8a2a9d4a7400ee63f53c59480ced059
Leontev Daniil <daniil.leontev@rtmis.ru>
2023-01-17 15:57:47 +0500

    закончил главу 41

--- js/functionClosure.js
+++ js/functionClosure.js
@@ -12,3 +12,25 @@ function logNumber() {
 number = 6;
 
 logNumber(); debugger
+
+
+// пример замыкания
+function createCounter(){
+    let counter = 0;
+
+    const myFynction = function(){
+        // counter = counter + 1;
+        return counter += 1;
+    };
+
+    return myFynction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1, c2 , c3);
+
+
